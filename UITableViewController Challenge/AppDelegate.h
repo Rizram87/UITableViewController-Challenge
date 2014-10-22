c738b9dca1d0ec3b8197ea310b39e23e487389d3
Rizki Ramadhan <rizkiramadhan@Rizkis-MacBook-Pro.local>
2014-10-22 22:39:20 +0700

    Initial Commit

---
+++ UITableViewController Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge
+//
+//  Created by Rizki Ramadhan on 10/22/14.
+//  Copyright (c) 2014 RizTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge
+//
+//  Created by Rizki Ramadhan on 10/22/14.
+//  Copyright (c) 2014 RizTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
